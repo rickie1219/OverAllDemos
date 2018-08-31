15863a5e1ac699be88b76400ff524a4e5b5ec469
Rickie_Lambert <lanou_0081@163.com>
2018-08-31 23:24:40 +0800

    实现图片轮播的功能

--- OverAllDemos/Modules/Mine/View/TestLoopViewCollectionCell/TestLoopViewCollectionView.m
+++ OverAllDemos/Modules/Mine/View/TestLoopViewCollectionCell/TestLoopViewCollectionView.m
@@ -34,6 +34,7 @@ NSString *const CZLoopViewCellId = @"CZLoopViewCellId";
         _urls = urls;
         self.backgroundColor = [UIColor colorWithRed:arc4random_uniform(256/255.0) green:arc4random_uniform(256/255.0) blue:arc4random_uniform(256/255.0) alpha:arc4random_uniform(256/255.0)];
         self.dataSource = self;
+        self.delegate = self;
         
         [self registerClass:[TestLoopViewCell class] forCellWithReuseIdentifier:CZLoopViewCellId];
         
@@ -61,7 +62,7 @@ NSString *const CZLoopViewCellId = @"CZLoopViewCellId";
 #pragma mark - 实现 collectionView 的数据源方法
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return _urls.count * 2;
+    return _urls.count * 100;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
@@ -74,6 +75,34 @@ NSString *const CZLoopViewCellId = @"CZLoopViewCellId";
     cell.url = _urls[indexPath.item % _urls.count];
     
     return cell;
+}
+
+#pragma mark - 实现 collectionView 的代理方法
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView
+{
+    
+    // 1. 获取当前停止的页面
+    NSInteger offset = scrollView.contentOffset.x / scrollView.bounds.size.width;
+    
+    NSLog(@"%zd", offset);
+    
+    // 2. 第0页， 跳转到， 第1组的第0页
+    // 最后一页， 跳转到第0组的最后一页
+    
+    if (offset == 0 || offset == ([self numberOfItemsInSection:0] - 1)) {
+        NSLog(@"---- %zd", offset);
+        
+        
+        // 最后一页
+        if (0 == offset) {
+            offset = _urls.count;
+        } else {
+            offset = _urls.count - 1;
+        }
+        
+        // 重新调整 contentOffset
+        scrollView.contentOffset = CGPointMake(offset * scrollView.bounds.size.width, 0);
+    }
 }
 
 
