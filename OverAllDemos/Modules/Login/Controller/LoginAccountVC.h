febe619565671108cd5d3b6fb75cc19b9e86e656
Rickie_Lambert <lanou_0081@163.com>
2018-09-14 15:26:12 +0800

    使用新的登录界面

---
+++ OverAllDemos/Modules/Login/Controller/LoginAccountVC.h
@@ -0,0 +1,13 @@
+//
+//  LoginAccountVC.h
+//  OverAllDemos
+//
+//  Created by Rickie_Lambert on 2018/9/14.
+//  Copyright © 2018年 RickieLambert. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface LoginAccountVC : BaseViewController
+
+@end
---
+++ OverAllDemos/Modules/Login/Controller/LoginAccountVC.m
@@ -0,0 +1,37 @@
+//
+//  LoginAccountVC.m
+//  OverAllDemos
+//
+//  Created by Rickie_Lambert on 2018/9/14.
+//  Copyright © 2018年 RickieLambert. All rights reserved.
+//
+
+#import "LoginAccountVC.h"
+
+@interface LoginAccountVC ()
+
+@end
+
+@implementation LoginAccountVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
